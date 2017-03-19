fe6bf6b181b6477c94bcc0f8c4853494a6f2057e
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-03-19 18:22:51 +0800

    Bump version to 1.0.0-alpha3

--- train.py
+++ train.py
@@ -4,7 +4,7 @@
 #
 # Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
 # Licensed under MIT
-# Version 1.0.0-alpha2
+# Version 1.0.0-alpha3
 
 import os
 from app import create_cv_im_instances_from_dir
